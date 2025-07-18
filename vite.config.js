310700ee991b8e8ed4ed5334acff0d47498569c8
Kenjin32icon <kariukilewis@gmail.com>
2025-07-18 13:09:13 +0300

    vit.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // Vite automatically loads .env files
+  // Variables prefixed with VITE_ are exposed to client-side code
+})
